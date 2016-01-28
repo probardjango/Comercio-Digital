5b233810f3709e61fe70c8a29d6a51e5a1b11ccf
probardjango <hola@teamcfe.com>
2016-01-28 20:37:42 +0100

    Vistas

--- src/productos/views.py
+++ src/productos/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def detail_view(request):
+	#1 objeto
+	template = "detail_view.html"
+	context = {}
+	print request
+	return render(request, template, context)
+
+def list_view(request):
+	#lista de objetos
+	template = "list_view.html"
+	context = {}
+	print request
+	return render(request, template, context)
