81e724a61abc59a582194a1f2e0265fa6ce2f1a6
probardjango <hola@teamcfe.com>
2016-01-30 01:38:01 +0100

    URLs y plantillas

--- src/comerciodigital/urls.py
+++ src/comerciodigital/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^detail/$', 'productos.views.detail_view', name='detail_view'),
+    url(r'^list/$', 'productos.views.list_view', name='list_view'),
 ]
