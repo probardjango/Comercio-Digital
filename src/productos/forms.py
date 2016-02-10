d1af9e1d4e40502c31ef658a6f1776247d9fb1bb
probardjango <hola@teamcfe.com>
2016-02-10 20:12:05 +0100

    validaciones formularios

--- src/productos/forms.py
+++ src/productos/forms.py
@@ -6,4 +6,20 @@ class ProductoForm(forms.Form):
 	descripcion = forms.CharField()
 	precio = forms.DecimalField()
 
-	
+
+	def clean_precio(self):
+		precio = self.cleaned_data.get("precio")
+		if precio <= 1.00:
+			raise forms.ValidationError("Precio tiene que ser mayor que $1.00")
+		elif precio > 99.99:
+			raise forms.ValidationError("Precio tiene que ser menor que $100.00")
+		else:
+			return precio
+
+
+	def clean_titulo(self):
+		titulo = self.cleaned_data.get("titulo")
+		if len(titulo) > 3:
+			return titulo
+		else: 
+			raise forms.ValidationError("Titulo debe contener mas de 3 caracteres")
